c153da101a54d3ea64ea0152763b0db70b890ac2
shujtasova <shujtasova99@gmail.com>
2024-02-20 20:18:11 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import video from "./food.mp4";
+import MyRecipesComponent from "./MyRecipesComponent";
 
 function App() {
+  const MY_ID = "b4d46036";
+  const MY_KEY = "d7c7bea0b9e13f78896e1ae84f4efb22";
+
+  const [mySearch, setMySearch] = useState("");
+  const [myRecipes, setMyRecipes] = useState([]);
+  const [wordSubmitted, setWordSubmitted] = useState('');
+
+  useEffect(() => {
+    const getRecipe = async () => {
+      const response = await fetch(
+        `https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${MY_ID}&app_key=${MY_KEY}`
+      );
+      const data = await response.json();
+      console.log(data);
+      setMyRecipes(data.hits);
+    };
+
+    getRecipe();
+  }, [wordSubmitted]);
+
+  const myRecipeSearch = (e) => {
+    setMySearch(e.target.value);
+  };
+
+  const finalSearch = (e) => {
+    e.preventDefault();
+    setWordSubmitted(mySearch);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <video autoPlay muted loop>
+          <source src={video} type="video/mp4" />
+        </video>
+        <h1>Find a Recipe</h1>
+      </div>
+
+      <div className="container">
+        <form onSubmit={finalSearch}>
+          <input
+            className="search"
+            onChange={myRecipeSearch}
+            value={mySearch}
+          />
+        </form>
+      </div>
+
+      <div className="container">
+        <button onClick={finalSearch}>
+          <img
+            src="https://img.icons8.com/fluency/48/000000/fry.png"
+            alt="icon"
+          />
+        </button>
+      </div>
+
+      {myRecipes.map((element, index) => (
+        <MyRecipesComponent key={index}
+          label={element.recipe.label}
+          image={element.recipe.image}
+          calories={element.recipe.calories}
+          ingredients={element.recipe.ingredientLines}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/MyRecipesComponent.js
@@ -0,0 +1,33 @@
+function MyRecipesComponent({ label, image, calories, ingredients }) {
+  return (
+    <div>
+      <div className="container">
+        <h2>{label}</h2>
+      </div>
+      <div className="container">
+        <img src={image} alt="dish" />
+      </div>
+
+<div className="center">
+      <ul className="container list">
+        {ingredients.map((ingredient, index) => (
+          <li key={index}>
+            <img
+              src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-check-multimedia-kiranshastry-gradient-kiranshastry.png "
+              alt="icon"
+              className="icon"
+            />
+            {ingredient}
+          </li>
+        ))}
+      </ul>
+      </div>
+
+      <div className="container">
+        <p>{calories.toFixed()} calories</p>
+      </div>
+    </div>
+  );
+}
+
+export default MyRecipesComponent;
